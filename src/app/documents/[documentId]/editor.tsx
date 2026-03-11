96ec528d414e5d07225b5335af181db70690d7d1
MJ <alswns8495@naver.com>
2026-03-11 15:20:12 +0900

    ✨ feat: tiptap editor 기본 기능 구현
    StarterKit 기반 에디터 초기 설정
    BulletList / OrderedList 리스트 기능 추가
    TaskList / TaskItem 체크리스트 기능 추가
    TableKit 테이블 기능 추가
    Image extension 이미지 삽입 기능 추가
    이미지 resize 기능 적용

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { TaskList, TaskItem } from "@tiptap/extension-list";
+import { TableKit } from "@tiptap/extension-table";
+import Image from "@tiptap/extension-image";
+import { useEditor, EditorContent } from "@tiptap/react";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,15 +15,32 @@ export const Editor = () => {
           "focus:outline-none print:bolder-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem,
+      TableKit,
+      Image.configure({
+        resize: {
+          enabled: true,
+          minWidth: 50,
+          minHeight: 50,
+          alwaysPreserveAspectRatio: true,
+        },
+      }),
+    ],
+    content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/600x400" />
+        <img src="https://placehold.co/800x400" />
+      `,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
   });
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
-      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-win-w-0">
+      <div className="min-w-max flex justify-center w-204 py-4 print:py-0 mx-auto print:w-full print:min-win-w-0">
         <EditorContent editor={editor} />
       </div>
     </div>
